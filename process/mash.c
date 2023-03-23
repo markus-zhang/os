b65f61db8131e6549d4c8a720510f7341abedb2b
markus-zhang <markus_zhang@hotmail.com>
2023-03-22 21:30:11 -0400

    refactored using global variables

--- process/mash.c
+++ process/mash.c
@@ -10,7 +10,7 @@
 #include <stdint.h>
 
 /*
-
+    TODO: Summarize the functionalities of MASH
 */
 
 #define DEBUG               true
@@ -18,19 +18,45 @@
 // Allow 64 strings in path initially, add 16 each time if needed
 #define PATH_INITIAL_SIZE   64
 #define PATH_INCREMENT_SIZE 16
+#define CWD_LEN             256
 
-int32_t init_path(char** path, int32_t* count);
-int32_t inspect_path(char** path, int32_t count);
-int32_t add_path(const char* newPath, char** path, int32_t* pathCount);
-int32_t remove_path(const char* oldPath, char** path, int32_t* pathCount);
+int32_t init_path();
+int32_t inspect_path();
+int32_t add_path(const char* newPath);
+int32_t remove_path(const char* oldPath);
+int32_t process_command(char* command);
+
+/*
+    Global variables
+    - cwd: current working directory
+        - type: C string
+    - path: collection of all directories in $PATH
+        - type: array of C strings
+    - pathCount: count of paths in $PATH
+        - type: int32_t
+        - comment: changes for each remove/add operation
+*/
+char* cwd;
+char** path;
+int32_t pathCount;
 
 int main(int argc, char* argv[]) {
-    char** path = malloc(PATH_INITIAL_SIZE * sizeof(char*));
-    int32_t pathCount = 0;
-    if (init_path(path, &pathCount) == EXIT_FAILURE) {
+    // Initiate CWD
+    cwd = malloc(CWD_LEN);
+    if (getcwd(cwd, CWD_LEN) == NULL) {
+        fprintf(stderr, "getcwd() failed\n");
+        return EXIT_FAILURE;
+    }
+    printf("CWD: %s\n", cwd);
+    
+    // Initiate PATH
+    path = malloc(PATH_INITIAL_SIZE * sizeof(char*));
+    pathCount = 0;
+    if (init_path() == EXIT_FAILURE) {
         fprintf(stderr, "init_path() failed\n");
         return EXIT_FAILURE;
     }
+    /*
     printf("init_path() successfully completed, %d paths loaded\n", pathCount);
     if (remove_path("/usr/games", path, &pathCount) != EXIT_SUCCESS) {
         printf("/usr/games not in path\n");
@@ -41,6 +67,7 @@ int main(int argc, char* argv[]) {
         printf("failed to add /usr/games, reached 64 paths\n");
     }
     inspect_path(path, pathCount);
+    */
 
     while(RUNNING) {
         fprintf(stdout, ">");
@@ -49,6 +76,13 @@ int main(int argc, char* argv[]) {
         if (getline(&input, &size, stdin) == -1) {
             fprintf(stderr, "getline() error\n");
         }
+        /*
+            Process user input
+            - built-in command:
+                - ls: list all files and directories under cwd
+        */
+        process_command(input);
+        
         printf(">%s\n", input);
     }
 
@@ -62,13 +96,13 @@ int main(int argc, char* argv[]) {
     exit(EXIT_SUCCESS);
 }
 
-int32_t init_path(char** path, int32_t* count) {
+int32_t init_path() {
     /*
-
+        TODO: Summarize the functionality of this function
     */
 
     // count must be NULL as we only initiate path at the beginning
-    if (*count > 0) {
+    if (pathCount > 0) {
         fprintf(stderr, "init_path(): path count must be 0\n");
         return EXIT_FAILURE;
     }
@@ -81,19 +115,19 @@ int32_t init_path(char** path, int32_t* count) {
     // Set count
     for (int i = 0;;i++) {
         if (s[i] == ':') {
-            (*count)++;
+            pathCount++;
         }
         if (s[i] == '\0') {
-            (*count)++;
+            pathCount++;
             break;
         }
     }
-    printf("There are a total of %d paths\n", *count);
+    printf("There are a total of %d paths\n", pathCount);
 
     // Start parsing
     int32_t start = 0;
     int32_t end = 0;
-    int32_t pathCount = 0;
+    int32_t count = 0;
     
     while (true) {
         if (s[end] == ':' || s[end] == '\0') {
@@ -105,9 +139,9 @@ int32_t init_path(char** path, int32_t* count) {
                 p[i] = s[start + i];
             }
             p[length] = '\0';
-            path[pathCount] = p;
+            path[count] = p;
             printf("%s\n", p);
-            pathCount++;
+            count++;
             if (s[end] == '\0') {
                 break;
             }
@@ -124,24 +158,24 @@ int32_t init_path(char** path, int32_t* count) {
     return EXIT_SUCCESS;
 }
 
-int32_t inspect_path(char** path, int32_t count) {
+int32_t inspect_path() {
     /*
-
+        TODO: Summarize the functionality of this function
     */
-    printf("There are a total of %d paths\n", count);
-    for (int i = 0; i < count; i++) {
+    printf("There are a total of %d paths\n", pathCount);
+    for (int i = 0; i < pathCount; i++) {
         printf("%s\n", path[i]);
     }
     
     return EXIT_SUCCESS;
 }
 
-int32_t add_path(const char* newPath, char** path, int32_t* pathCount) {
+int32_t add_path(const char* newPath) {
     /*
         If more than 64 already then refuses to add
         TODO: re allocate 64+16 and copy to new array
     */
-    if (*pathCount >= PATH_INCREMENT_SIZE) {
+    if (pathCount >= PATH_INCREMENT_SIZE) {
         return EXIT_FAILURE;
     }
     size_t len = strlen(newPath);
@@ -150,26 +184,32 @@ int32_t add_path(const char* newPath, char** path, int32_t* pathCount) {
         temp[i] = newPath[i];
     }
     temp[len] = '\0';
-    path[*pathCount] = temp;
-    (*pathCount)++;
+    path[pathCount] = temp;
+    pathCount++;
     return EXIT_SUCCESS;
 }
 
-int32_t remove_path(const char* oldPath, char** path, int32_t* pathCount) {
+int32_t remove_path(const char* oldPath) {
     /*
         Seach for oldPath in path;
         Once found at position i then remove it
     */
-    for (int i = 0; i < *pathCount; i++) {
+    for (int i = 0; i < pathCount; i++) {
         if (strcmp(path[i], oldPath) == 0) {
             // remove by moving pointers after
-            for (int j = i; j < *pathCount - 1; j++) {
+            for (int j = i; j < pathCount - 1; j++) {
                 path[j] = path[j + 1];
             }
-            path[*pathCount - 1] = NULL;
-            (*pathCount)--;
+            path[pathCount - 1] = NULL;
+            (pathCount)--;
             return EXIT_SUCCESS;
         }
     }
     return EXIT_FAILURE;
+}
+
+int32_t process_command(char* command) {
+    /*
+        TODO: Summarize the functionality of this function
+    */
 }
