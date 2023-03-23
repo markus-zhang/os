110dafb26e51358b1d977c8cb8ac0c0619e198a3
markus-zhang <markus_zhang@hotmail.com>
2023-03-22 23:26:25 -0400

    added insertion sort to sort ls output

--- process/mash.c
+++ process/mash.c
@@ -222,13 +222,55 @@ int32_t process_command(char* command) {
     DIR* d;
     struct dirent* dir;
     d = opendir(".");
+    int32_t fileCount = 0;
     if (d) {
         while ((dir = readdir(d)) != NULL) {
             if (dir->d_type == DT_REG) {
-                printf("%s  ", dir->d_name);
+                fileCount++;
             }
         }
+        // Now load all filenames into an array
+        // First rewind to beginning of folder
+        rewinddir(d);
+        char** filename = malloc(fileCount * sizeof(char*));
+        fileCount = 0;
+        while ((dir = readdir(d)) != NULL) {
+            if (dir->d_type == DT_REG) {
+                int len = strlen(dir->d_name);
+                char* n = malloc(len + 1);
+                for (int i = 0; i < len; i++) {
+                    n[i] = (dir->d_name)[i];
+                }
+                n[len] = '\0';
+                filename[fileCount] = n;
+                fileCount++;
+            }
+        }
+        // Insertion sort, use strcasecmp() to compare
+        // First compare 1 with 0, obtain a stable 0~1
+        // Next compare 2 with 0~1, obtain a stable 0~2
+        // ...until compare n-1 with 0~n-2, obtain a stable 0~n-1, done
+        int32_t next = 1;
+        while (next < fileCount) {
+            int32_t walker = next;
+            while (walker > 0 && strcasecmp(filename[walker - 1], filename[walker]) > 0) {
+                char* temp = filename[walker];
+                filename[walker] = filename[walker - 1];
+                filename[walker - 1] = temp;
+                walker--;
+            }
+            next++;
+        }
+        // test print
+        for (int i = 0; i < fileCount; i++) {
+            printf("%s  ", filename[i]);
+        }
         printf("\n");
+        // TODO: free strings in filename and then free filename
+        for (int i = 0; i < fileCount; i++) {
+            free(filename[i]);
+        }
+        free(filename);
         closedir(d);
     }
     return EXIT_SUCCESS;
