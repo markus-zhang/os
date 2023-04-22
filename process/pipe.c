82f47e3674ebd6c197cefed08ab8c2e3f261bf64
markus-zhang <markus_zhang@hotmail.com>
2023-04-21 23:31:52 -0400

    more research about pipes

--- process/pipe.c
+++ process/pipe.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <assert.h>
 
 int main() {
     int pipefd[2];
@@ -32,9 +33,11 @@ int main() {
 
         // Close the read end, as we don't need it in this process
         close(pipefd[0]);
+        close(pipefd_next[0]);
+        close(pipefd_next[1]);
 
         // Execute "cat ./file.txt"
-        execlp("echo", "echo", "asdsasdsadasdsadsad cat", (char *)NULL);
+        execlp("echo", "echo", "asdsasdsadasdsadsadcat", (char *)NULL);
         perror("execlp");
         exit(EXIT_FAILURE);
     }
@@ -49,25 +52,26 @@ int main() {
     if (pid_B == 0) { // Child process B
         // Redirect stdin to the pipe's read end
         dup2(pipefd[0], STDIN_FILENO);
+        dup2(pipefd_next[1], STDOUT_FILENO);
+        close(pipefd_next[0]);
 
         // Close the write end, as we don't need it in this process
         close(pipefd[1]);
         
-        dup2(pipefd_next[1], STDOUT_FILENO);
-        close(pipefd_next[0]);
-
+        
+        
         // Execute "grep password"
         execlp("grep", "grep", "cat", (char *)NULL);
         perror("execlp");
         exit(EXIT_FAILURE);
     }
-    
-    // Fork child process C
+
     pid_C = fork();
     if (pid_C == -1) {
         perror("fork");
         exit(EXIT_FAILURE);
     }
+
 
     if (pid_C == 0) { // Child process B
         // Redirect stdin to the pipe's read end
@@ -75,22 +79,32 @@ int main() {
 
         // Close the write end, as we don't need it in this process
         close(pipefd_next[1]);
+        // commenting the follow line is fine
+        // close(pipefd[0]);
+        // commenting the next line is NOT fine
+        // close(pipefd[1]);
 
         // Execute "grep password"
         execlp("wc", "wc", (char *)NULL);
         perror("wc");
         exit(EXIT_FAILURE);
     }
-
+    
     // Close both ends of the pipe in the parent process
     close(pipefd[0]);
     close(pipefd[1]);
     close(pipefd_next[0]);
     close(pipefd_next[1]);
 
+
     // Wait for both child processes to complete
     waitpid(pid_A, NULL, 0);
     waitpid(pid_B, NULL, 0);
+
+     // Fork child process C
+    
+
+
     waitpid(pid_C, NULL, 0);
 
     return 0;
