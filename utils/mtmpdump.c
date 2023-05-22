b49e258712d0003780f370492fcc00933cbbcc06
markus-zhang <markus_zhang@hotmail.com>
2023-05-22 18:28:01 -0400

    random code

---
+++ utils/mtmpdump.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        fprintf(stderr, "usage: mtmpdump </path/to/utmp>\n");
+        exit(1);
+    }
+
+    int fd;
+    char buffer[65536];
+
+    // open devices
+    if ((fd = open(argv[1], O_WRONLY)) == -1) {
+        perror(argv[1]);
+        exit(1);
+    }
+
+    // write to device
+    while (fgets(buffer, BUFSIZ, stdin) != NULL) {
+        if (write(fd, buffer, strlen(buffer)) == -1) {
+            break;
+        }
+    }
+
+    close(fd);
+
+    return 0;
+}
